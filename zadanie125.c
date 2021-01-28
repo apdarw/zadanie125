5901faeef819581d44ece6fac3d9f045c748ad02
Adrian Pawlowski <apdarw@gmail.com>
2021-01-28 19:24:22 +0100

    Dodanie funkcjonalnosci zwiazanej z obliczaniem objetosci ostroslupa.

--- zadanie125.c
+++ zadanie125.c
@@ -2,27 +2,38 @@
 #include <stdlib.h>
 #include <math.h>
 
-int objetosc(int a, int b, int H)
+int objetosc_prostopadl(int a, int b, int H)
 {
-    int objetosc;
-    objetosc = a * b * H;
+    int V;
+    V = a * b * H;
 
-    return objetosc;
+    return V;
+}
+
+int objetosc_ostroslup(int a, int b, int H)
+{
+    int V;
+    V = (a * a * H) / 3;
+
+    return V;
 }
 
 int main(void)
 {
-    int a, b, H, V;
+    int a, b, H, V1, V2;
 
-    printf("Podaj dlugosc boku a, szerokosc boku b oraz wysokosc H: \n");
+    printf("Podaj kolejno w wierszach dlugosc podstawy, szerokosc podstawy oraz wysokosc figury: \n");
     scanf("%d %d %d", &a, &b, &H);
 
     if (a <= 0 || b <= 0 || H <= 0){
         printf("Podano zle wartosci!!");
 
     } else {
-        V = objetosc(a, b, H);
-        printf("Objetosc prostopadloscianu wynosi: %d \n", V);
+        V1 = objetosc_prostopadl(a, b, H);
+        V2 = objetosc_ostroslup(a, b, H);
+
+        printf("Objetosc prostopadloscianu wynosi: %d \n", V1);
+        printf("Objetosc ostroslupa wynosi: %d \n", V2);
     }
 return 0;
 }
